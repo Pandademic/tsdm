1a90345631af363e7d09d633a246d80ba8fc9b7e
Pandademic <atharv.gupte510@gmail.com>
2022-05-27 13:59:22 -0400

    Add a setup command to setup the tsdm dirs
    The dir is called ".tsdm" in the user's home directory

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pandademic/tsdm
 
 go 1.18
 
-require github.com/pandademic/raspberry v0.0.0-20220527153227-2ccc8e17df90 // indirect
+require (
+	github.com/Pandademic/luve v0.0.0-20220419225134-5ff3657140af // indirect
+	github.com/pandademic/raspberry v0.0.0-20220527153227-2ccc8e17df90 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
+// TODO ADD ENV VARS TO PATH FOR SUCCESS
 import (
-  "github.com/pandademic/raspberry"
+	"fmt"
+	"github.com/Pandademic/luve"
+	"github.com/pandademic/raspberry"
+	"os"
 )
 
-func main(){
-  help := `
+var (
+	UserHomeDir string
+)
+
+func setupDirs() {
+	doesDirExist, err := os.Stat(UserHomeDir + "/.tsdm")
+	UserHomeDir, _ = os.UserHomeDir()
+	luve.Luve(doesDirExist)
+	if os.IsNotExist(err) {
+		if err := os.Mkdir(UserHomeDir+"/.tsdm", os.ModePerm); err != nil {
+			fmt.Println("error creating directorys")
+			fmt.Println("ERROR:", err)
+			os.Exit(1)
+		} else {
+			fmt.Println("Success!")
+		}
+	} else {
+		fmt.Println("Directory already exists!")
+		os.Exit(1)
+	}
+}
+func main() {
+	help := `
            TSDM HELP 
          --------------
 
@@ -20,13 +45,15 @@ func main(){
          - version
 
          - -v 
-
+        
+         - setup
 
   `
-  cli := raspberry.Cli{
-    AcceptedCommands: []string{"-v","version","-h","help"},
-    HelpMsg:help,
-    Version: 0.1,
-  }
-  cli.Setup()
+	cli := raspberry.Cli{
+		AcceptedCommands: []string{"-v", "version", "-h", "help", "setup"},
+		HelpMsg:          help,
+		Version:          0.1,
+	}
+	cli.Setup()
+	cli.SetHandler("setup", setupDirs)
 }
