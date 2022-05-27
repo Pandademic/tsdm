99521183e05dc908061c23e94b7e8aa63e49b7c6
Pandademic <atharv.gupte510@gmail.com>
2022-05-27 15:05:19 -0400

    Add a 'get' command
    The 'get' command will get a repo from a specified source and store it
    in the '.tsdm' dir. You can only have one such repo at a time.

--- main.go
+++ main.go
@@ -1,31 +1,43 @@
 package main
 
-// TODO ADD ENV VARS TO PATH FOR SUCCESS
 import (
 	"fmt"
 	"github.com/Pandademic/luve"
+	"github.com/go-git/go-git/v5"
 	"github.com/pandademic/raspberry"
 	"os"
 )
 
 var (
 	UserHomeDir string
+	UserTsdmDir string
 )
 
 func setupDirs() {
-	doesDirExist, err := os.Stat(UserHomeDir + "/.tsdm")
-	UserHomeDir, _ = os.UserHomeDir()
+	doesDirExist, err := os.Stat(UserTsdmDir)
 	luve.Luve(doesDirExist)
 	if os.IsNotExist(err) {
-		if err := os.Mkdir(UserHomeDir+"/.tsdm", os.ModePerm); err != nil {
+		if err := os.Mkdir(UserTsdmDir, os.ModePerm); err != nil {
 			fmt.Println("error creating directorys")
-			fmt.Println("ERROR:", err)
+			luve.Luve(err)
 			os.Exit(1)
 		} else {
 			fmt.Println("Success!")
 		}
 	} else {
 		fmt.Println("Directory already exists!")
+		os.Exit(1)
+	}
+}
+func getRepo() {
+	cloneDir := UserTsdmDir + string(os.PathSeparator) + "dotfile-repo"
+	fmt.Println("getting a repository from ", raspberry.Args[0], " to: ", cloneDir)
+	_, err := git.PlainClone(cloneDir, false, &git.CloneOptions{
+		URL:      raspberry.Args[0],
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
@@ -49,11 +61,14 @@ func main() {
          - setup
 
   `
+	UserHomeDir, _ = os.UserHomeDir()
+	UserTsdmDir = UserHomeDir + string(os.PathSeparator) + ".tsdm"
 	cli := raspberry.Cli{
-		AcceptedCommands: []string{"-v", "version", "-h", "help", "setup"},
+		AcceptedCommands: []string{"-v", "version", "-h", "help", "setup", "get"},
 		HelpMsg:          help,
 		Version:          0.1,
 	}
 	cli.Setup()
 	cli.SetHandler("setup", setupDirs)
+	cli.SetHandler("get", getRepo)
 }
